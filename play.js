b7ca4d817300b1f2a13357ce76fa1cb52c74c0d2
PandaBurglar <zane.bokhari@gmail.com>
2021-12-03 20:54:34 -0500

    setting up multiplayer snake game

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  // create an object called conn
+  const conn = net.createConnection({
+    host: "localhost", 
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("you ded cuz you idled \n")
+  });
+
+  return conn; // return represents the connection with the server
+};
+
+console.log("Connecting ...");
+connect();
