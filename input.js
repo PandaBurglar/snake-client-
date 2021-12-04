ddfdc3b768add0b9b86a1a4ff98b469aa4db1d3c
PandaBurglar <zane.bokhari@gmail.com>
2021-12-04 15:44:36 -0500

    TCP connection and UI set up for multiplayer snake game

--- input.js
+++ input.js
@@ -1,19 +1,40 @@
+// Stores the active TCP connection object.
+let connection;
 
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-    stdin.on("data", handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
-module.exports = setupInput;
+const handleUserInput = function (key) {
+  // if ctrl c is pressed, exit the game
+    if (key === '\u0003') {
+      process.exit();
+    }
+    else if (key === 'w') {
+      console.log("Move: up");
+      connection.write("Move: up");
+    }
+    else if (key === 'a') {
+      console.log("Move: left");
+      connection.write("Move: left");
+    }
+    else if (key === 's') {
+      console.log("Move: down");
+      connection.write("Move: down");
+    }
+    else if (key === 'd') {
+      console.log("Move: right");
+      connection.write("Move: right");
+    }
+};
+
+module.exports = {setupInput};
