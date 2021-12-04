86053be6d7df3a90939f452c1722a168b48b869a
PandaBurglar <zane.bokhari@gmail.com>
2021-12-03 21:06:56 -0500

    client set up for multiplayer snake game

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+  // create an object called conn
+  const conn = net.createConnection({
+    host: "localhost", 
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("you ded cuz you idled \n")
+  });
+
+  return conn; // return represents the connection with the server
+};
+
+module.exports = connect;
