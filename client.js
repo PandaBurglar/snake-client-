3e7f5a6ea50cce0eb2a5249c5ea06114d25f9b8c
PandaBurglar <zane.bokhari@gmail.com>
2021-12-03 21:14:30 -0500

    client set up for multiplayer snake game

--- client.js
+++ client.js
@@ -6,7 +6,10 @@ const connect = function () {
     host: "localhost", 
     port: 50541,
   });
-
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: ZPB");
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
